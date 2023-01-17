5f01234d35355dda1b55e5e651aafc1bba2cc082
Dmitry Shpika <scrptn@gmail.com>
2023-01-18 00:56:31 +0400

    Use hex numbers instead of strings

--- scripts/han.js
+++ scripts/han.js
@@ -1,44 +1,44 @@
 const HAN_RANGES = [
-  ['4E00', '9FFF'], // CJK Unified Ideographs
-  ['3400', '4DBF'], // CJK Unified Ideographs Extension A
-  ['20000', '2A6DF'], // CJK Unified Ideographs Extension B
-  ['2A700', '2B739'], // CJK Unified Ideographs Extension C
-  ['2B740', '2B81D'], // CJK Unified Ideographs Extension D
-  ['2B820', '2CEA1'], // CJK Unified Ideographs Extension E
-  ['2CEB0', '2EBE0'], // CJK Unified Ideographs Extension F
-  ['30000', '3134A'], // CJK Unified Ideographs Extension G
-  ['31350', '323AF'], // CJK Unified Ideographs Extension H
-  ['2E80', '2EFF'], // CJK Radicals Supplement
-  ['2F00', '2FDF'], // Kangxi Radicals
-  ['F900', 'FAFF'], // CJK Compatibility Ideographs
-  ['2F800', '2FA1F'], // CJK Compatibility Ideographs Supplement
+  [0x4e00, 0x9fff], // CJK Unified Ideographs
+  [0x3400, 0x4dbf], // CJK Unified Ideographs Extension A
+  [0x20000, 0x2a6df], // CJK Unified Ideographs Extension B
+  [0x2a700, 0x2b739], // CJK Unified Ideographs Extension C
+  [0x2b740, 0x2b81d], // CJK Unified Ideographs Extension D
+  [0x2b820, 0x2cea1], // CJK Unified Ideographs Extension E
+  [0x2ceb0, 0x2ebe0], // CJK Unified Ideographs Extension F
+  [0x30000, 0x3134a], // CJK Unified Ideographs Extension G
+  [0x31350, 0x323af], // CJK Unified Ideographs Extension H
+  [0x2e80, 0x2eff], // CJK Radicals Supplement
+  [0x2f00, 0x2fdf], // Kangxi Radicals
+  [0xf900, 0xfaff], // CJK Compatibility Ideographs
+  [0x2f800, 0x2fa1f], // CJK Compatibility Ideographs Supplement
 ];
 
 const HAN_EXT_RANGES = [
   // CJK Symbols and Punctuation:
-  ['3005'], // 々 IDEOGRAPHIC ITERATION MARK
-  ['3006'], // 〆 IDEOGRAPHIC CLOSING MARK
+  [0x3005], // 々 IDEOGRAPHIC ITERATION MARK
+  [0x3006], // 〆 IDEOGRAPHIC CLOSING MARK
 
   // CJK Strokes:
-  ['31C0', '31EF'],
+  [0x31c0, 0x31ef],
 
   // Enclosed CJK Letters and Months:
-  ['3220', '3243'], // parenthesized
-  ['3244', '3247'], // circled
-  ['3280', '32B0'], // circled
-  ['32C0', '32CB'], // telegraph symbols for months
-  ['32FF'], // square era name: REIWA
+  [0x3220, 0x3243], // parenthesized
+  [0x3244, 0x3247], // circled
+  [0x3280, 0x32b0], // circled
+  [0x32c0, 0x32cb], // telegraph symbols for months
+  [0x32ff], // square era name: REIWA
 
   // CJK Compatibility:
-  ['3358', '3370'], // telegraph symbols for hours
-  ['337B', '337E'], // square era names: HEISEI, SYOUWA, TAISYOU, MEIZI
-  ['337F'], // square: CORPORATION
-  ['33E0', '33FE'], // telegraph symbols for days (1st-31st)
+  [0x3358, 0x3370], // telegraph symbols for hours
+  [0x337b, 0x337e], // square era names: HEISEI, SYOUWA, TAISYOU, MEIZI
+  [0x337f], // square: CORPORATION
+  [0x33e0, 0x33fe], // telegraph symbols for days (1st-31st)
 
   // Enclosed Ideographic Supplement:
-  ['1F210', '1F23B'], // enclosed in squares
-  ['1F240', '1F248'], // tortoise shell bracketed
-  ['1F250', '1F251'], // circled
+  [0x1f210, 0x1f23b], // enclosed in squares
+  [0x1f240, 0x1f248], // tortoise shell bracketed
+  [0x1f250, 0x1f251], // circled
 ];
 
 module.exports = {
