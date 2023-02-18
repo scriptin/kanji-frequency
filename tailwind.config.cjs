160fd3171f8cc0124517a639ef8499e7968a1f97
Dmitry Shpika <scrptn@gmail.com>
2023-02-18 19:16:50 +0400

    Add tailwind config for markdown sections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,36 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
     require('@tailwindcss/container-queries'),
     function ({ addBase, theme }) {
       addBase({
         // Basic links
-        'a': {
+        a: {
           textDecoration: 'underline',
           textDecorationColor: theme('textColor.sky[500]'),
-          textUnderlineOffset: theme('textUnderlineOffset.2'),
+          textUnderlineOffset: theme('textUnderlineOffset[2]'),
         },
         'a:hover': {
           textDecoration: 'underline',
           textDecorationColor: theme('textColor.sky[900]'),
         },
         // Inverted links
-        'a.link-inverted': {
-          textDecoration: 'underline',
+        'a.link-inverted, .box-inverted a': {
           textDecorationColor: theme('textColor.sky[300]'),
-          textUnderlineOffset: theme('textUnderlineOffset.2'),
+          textUnderlineOffset: theme('textUnderlineOffset[2]'),
         },
-        'a.link-inverted:hover': {
-          textDecoration: 'underline',
+        'a.link-inverted:hover, .box-inverted a:hover': {
           textDecorationColor: theme('textColor.sky[100]'),
         },
         // Plain links - no underline or other decorations
         'a.link-plain': {
           textDecoration: 'inherit',
+        },
+        // Headings
+        '.markdown h2, .markdown h3': {
+          fontWeight: theme('fontWeight.black'),
+          marginBottom: theme('margin[1]'),
+        },
+        '.markdown h2': {
+          fontSize: theme('fontSize.2xl.'),
+          lineHeight: theme('fontSize.2xl.[1].lineHeight'),
+        },
+        '.markdown h3': {
+          fontSize: theme('fontSize.xl'),
+          lineHeight: theme('fontSize.xl[1].lineHeight'),
+        },
+        '.markdown p + h2, .markdown p + h3': {
+          marginTop: theme('margin[4]'),
+        },
+        // Paragraph
+        '.markdown p': {
+          marginBottom: theme('margin[4]'),
+        },
+        '.markdown p:last-child': {
+          marginBottom: 0,
         },
       });
     },
