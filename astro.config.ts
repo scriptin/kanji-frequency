d867001c6ebdeedc767b6929f94c26137a1b993d
Dmitry Shpika <scrptn@gmail.com>
2023-02-18 19:57:04 +0400

    Convert index to MDX

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 import dsv from '@rollup/plugin-dsv';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://scriptin.github.io',
   base: '/kanji-frequency',
-  integrations: [
-    tailwind(),
-  ],
+  integrations: [tailwind(), mdx()],
   vite: {
-    plugins: [
-      dsv(),
-    ],
+    plugins: [dsv()],
   },
 });
