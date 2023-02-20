96fd72e3099027d975fc884f72a09112bd7589c9
Dmitry Shpika <scrptn@gmail.com>
2023-02-20 20:10:37 +0400

    More digrams; Improve docs; Refactor

---
+++ src/components/chart/colors.ts
@@ -0,0 +1,3 @@
+export const AOZORA_COLOR = 'rgb(3 105 161)'; // text-sky-700
+export const WIKIPEDIA_COLOR = 'rgb(185 28 28)'; // text-red-700
+export const NEWS_COLOR = 'rgb(21 128 61)'; // text-green-700
