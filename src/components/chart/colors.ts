e045b227d39ac13aa465cbe14340bca2b3d2c402
Dmitry Shpika <scrptn@gmail.com>
2023-02-20 22:40:24 +0400

    Update diagram colors

--- src/components/chart/colors.ts
+++ src/components/chart/colors.ts
@@ -1,3 +1,4 @@
-export const AOZORA_COLOR = 'rgb(3 105 161)'; // text-sky-700
-export const WIKIPEDIA_COLOR = 'rgb(185 28 28)'; // text-red-700
-export const NEWS_COLOR = 'rgb(21 128 61)'; // text-green-700
+// https://tailwindcss.com/docs/customizing-colors
+export const AOZORA_COLOR = '#0284c7'; // text-sky-600
+export const WIKIPEDIA_COLOR = '#dc2626'; // text-red-600
+export const NEWS_COLOR = '#16a34a'; // text-green-600
